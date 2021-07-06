b57686901b7cbbeb56381bd9165dfd2425fe8763
tukezor <petuukka@gmail.com>
2021-07-06 20:24:42 +0300

    need to specify length if offset

--- backend/src/application/torrentEngine/fileStream.ts
+++ backend/src/application/torrentEngine/fileStream.ts
@@ -1,5 +1,6 @@
 import { TorrentFile } from 'application/torrentEngine/file';
 import { Readable } from 'stream';
+import Debug from 'debug';
 
 export class FileStream extends Readable {
 	startPiece: number;
@@ -9,10 +10,12 @@ export class FileStream extends Readable {
 	length: number;
 	currentPiece: number;
 	file: TorrentFile;
+	debug = Debug('filestream');
 
 	constructor(file: TorrentFile, start: number, end: number) {
 		super();
 
+		this.debug(`Filestream: new ${start}-${end}`);
 		this.startPiece = Math.floor(
 			(file.options.offset + start) / file.options.chunkLength
 		);
@@ -27,22 +30,33 @@ export class FileStream extends Readable {
 	}
 
 	_read = (): void => {
+		this.debug('Filestream: read');
 		const piecesToQueue = Math.min(4, this.endPiece - this.currentPiece + 1);
 		this.file.instance.prioritize(this.currentPiece, piecesToQueue);
 		const opts = { offset: 0, length: this.file.options.chunkLength };
 		if (this.currentPiece === this.startPiece && this.startOffset) {
 			opts.offset = this.startOffset;
+			opts.length = opts.length - this.startOffset;
 		}
 		if (this.currentPiece === this.endPiece && this.endLength) {
 			opts.length = this.endLength;
 		}
 		const readPiece = () => {
+			this.debug(`Filestream: read piece ${this.currentPiece}`);
 			this.file.store.get(this.currentPiece, opts, (err, buffer) => {
 				if (err) {
+					this.debug(
+						`Filestream: destroy`,
+						err,
+						opts,
+						this.startPiece,
+						this.endPiece
+					);
 					this.destroy();
 				}
 				this.push(buffer);
 				if (this.currentPiece === this.endPiece) {
+					this.debug(`Filestream: push null`);
 					this.push(null);
 				} else {
 					this.currentPiece++;
